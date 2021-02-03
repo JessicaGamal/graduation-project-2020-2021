05cbb01eb2c967fce79fc4c80a60c70474bcc3db
sara-hussein <sh184610@gmail.com>
2021-02-03 15:51:59 +0200

    change db name

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-const db_url ="mongodb://localhost:27017/register"
+const db_url ="mongodb://localhost:27017/College-System"
 const connecttodb = async()=>{
     mongoose.connect(db_url,{
         useNewUrlParser:true,
