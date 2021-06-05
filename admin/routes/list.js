310dd5f0b8466e1ed2719d394a18e00dd48dd156
sara-hussein <sh184610@gmail.com>
2021-06-06 01:20:16 +0200

    admin

--- admin/routes/list.js
+++ admin/routes/list.js
@@ -2,7 +2,7 @@ const router =require('express').Router()
 const homecontroller = require('../controller/list')
 const User=require('../model/authmodel')
 
-router.get('/',homecontroller.getuser)
+router.get('/list',homecontroller.getuser)
 
 router.get("/mongooses/:id/destroy",User.delete)
 module.exports = router
